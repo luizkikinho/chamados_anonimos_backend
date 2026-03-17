08c3970ce16ff21b7a61d4001138ec9c1196ddbe
luizkikinho <luiz.caetano2@fatec.sp.gov.br>
2026-03-17 11:01:58 -0300

    tentando corrigir erro de webhook

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ const app = express(); // inicializa o servidor
 app.use(express.json());
 
 app.post("/webhook", async (req, res) => {
-	// recebe ('request') o JSON da Evolution
-	const result = await processWebhook(req.body);
-	if (result) {
-		console.log(
-			`[LOG TG] Processado ID: ${result.anonId} | Ação: ${result.action}`,
-		);
-	}
+	// LOG DE EMERGÊNCIA: Isso vai mostrar QUALQUER coisa que bater no servidor
+	console.log("------------------------------------");
+	console.log("📩 NOVA REQUISIÇÃO RECEBIDA!");
+	console.log("CORPO DA REQUISIÇÃO:", JSON.stringify(req.body, null, 2));
+	console.log("------------------------------------");
 
-	// Responde 200 para não ficar dando loop de mensagem
-	res.status(200).send("OK");
+	const result = await processWebhook(req.body);
+	res.status(200).json(result);
 });
 
 const PORT = 3000;
