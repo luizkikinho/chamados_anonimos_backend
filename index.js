8a7a3dfe64f3bab55136dca3394800cd8bde4f03
luizkikinho <luiz.caetano2@fatec.sp.gov.br>
2026-03-17 13:16:16 -0300

    Removido o Log de testes do index.js

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const app = express(); // inicializa o servidor
 app.use(express.json());
 
 app.post("/webhook", async (req, res) => {
-	// LOG DE EMERGÊNCIA: Isso vai mostrar QUALQUER coisa que bater no servidor
-	console.log("------------------------------------");
-	console.log("📩 NOVA REQUISIÇÃO RECEBIDA!");
-	console.log("CORPO DA REQUISIÇÃO:", JSON.stringify(req.body, null, 2));
-	console.log("------------------------------------");
-
 	const result = await processWebhook(req.body);
 	res.status(200).json(result);
 });
