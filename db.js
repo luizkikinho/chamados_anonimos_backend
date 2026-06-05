97c14e1f4784e1706cbf21740b970741824ee863
luizkikinho <luizcharleauxecaetano@gmail.com>
2026-06-05 11:29:06 -0300

    feat: update Supabase client configuration and standardize string quotes

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Apenas iremos utilizar a função 'createClient' do SDK do Supabase,
 // demarcado pelos colchetes {}
-const { createClient } = require('@supabase/supabase-js');
+const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+// const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE;
 const supabase = createClient(supabaseUrl, supabaseKey); // A conexão é criada
 
 module.exports = supabase; // Exporta a conexão para usar em outro lugar
