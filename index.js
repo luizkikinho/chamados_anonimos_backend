7682fdb25ef909dee5f8be3d01ebeb4a9c858d6f
luizkikinho <luizcharleauxecaetano@gmail.com>
2026-06-07 12:48:13 -0300

    refactor: remove a função de ping por conta do desuso

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
 const express = require("express");
-const { processWebhook } = require("./functions");
-const { exec } = require("child_process");
+const {processWebhook} = require("./functions");
+const {exec} = require("child_process");
 const app = express(); // Ativa o servidor
 
 app.use(express.json());
 
 app.post("/webhook", async (req, res) => {
-  const result = await processWebhook(req.body);
-  res.status(200).json(result);
-});
-
-app.post("/ping", (req, res) => {
-  console.log("[KEEP-ALIVE] Recebido o ping de rotina.");
-  res.status(200).send("pong");
+    const result = await processWebhook(req.body);
+    res.status(200).json(result);
 });
 
 app.post("/deploy-hook", (req, res) => {
-  console.log("[DEPLOY] Recebido sinal do GitHub. Baixando atualizações...");
-  res.status(200).send("Deploy iniciado.");
-  exec(
-    "git pull origin main && pm2 restart whatsapp-bot",
-    (err, stdout, stderr) => {
-      if (err) {
-        console.error(`[DEPLOY ERRO] ${err}`);
-        return;
-      }
-      console.log(`[DEPLOY SUCESSO] Sistema atualizado e reiniciado!`);
-      if (stdout) console.log(`[OUTPUT] ${stdout}`);
-    },
-  );
+    console.log("[DEPLOY] Recebido sinal do GitHub. Baixando atualizações...");
+    res.status(200).send("Deploy iniciado.");
+    exec(
+        "git pull origin main && pm2 restart whatsapp-bot",
+        (err, stdout) => {
+            if (err) {
+                console.error(`[DEPLOY ERRO] ${err}`);
+                return;
+            }
+            console.log(`[DEPLOY SUCESSO] Sistema atualizado e reiniciado!`);
+            if (stdout) console.log(`[OUTPUT] ${stdout}`);
+        },
+    );
 });
 
 const PORT = 8000;
 app.listen(PORT, () => {
-  console.log(`Servidor de triagem iniciado na porta ${PORT}...\n`);
+    console.log(`Servidor de triagem iniciado na porta ${PORT}...\n`);
 });
